cffe28ccfd118113df1ae46aa056bd5b5133f6c9
syntakks <syntakks@gmail.com>
2020-01-22 20:32:57 -0600

    Initial Commit
    First Commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 targetOffset;
+    public float moveSpeed; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + targetOffset, moveSpeed * Time.deltaTime); 
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator; 
+    float horizontal;
+    float vertical;
+    Vector3 movement; 
+    public float moveSpeed;
+    float animationSpeed;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementInput();
+        HandleAnimation(); 
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void HandleMovementInput()
+    {
+        horizontal = CrossPlatformInputManager.GetAxis("Horizontal"); 
+        vertical = CrossPlatformInputManager.GetAxis("Vertical");
+        movement = new Vector3(horizontal, 0, vertical).normalized; 
+    }
+
+    private bool UpdateAngles()
+    {
+        return horizontal != 0 || vertical != 0; 
+    }
+
+    private void Move()
+    {
+        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+        if (UpdateAngles())
+        {
+            transform.eulerAngles = new Vector3(0, Mathf.Atan2(horizontal, vertical) * 180 / Mathf.PI, 0);
+        }
+    }
+
+    private void HandleAnimation()
+    {
+        float horizontalMag = Mathf.Abs(horizontal);
+        float verticalMag = Mathf.Abs(vertical);
+        animationSpeed = Mathf.Max(horizontalMag, verticalMag); 
+        animator.SetFloat("Speed", animationSpeed); 
+    }
+    
+}
